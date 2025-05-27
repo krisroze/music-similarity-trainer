e11ddcdff0a99d52bfc634bc13a5477e6007f11b
krisroze <89241274+krisroze@users.noreply.github.com>
2025-05-27 12:06:05 +0700

    Update server.js to port to Azure

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 
 // Spotify API credentials
 let config;
